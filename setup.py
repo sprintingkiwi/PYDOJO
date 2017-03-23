81994e4448e2c90e5f410c90df4ba2d1baf8a6db
Alessandro Norfo <sprintingkiwi@gmail.com>
2017-03-23 11:18:00 +0100

    0.9 version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pydojo',
   packages = ['pydojo'], # this must be the same as the name above
-  version = '0.8',
+  version = '0.9',
   description = 'A playful way to learn coding with Python',
   author = 'Alessandro Norfo',
   author_email = 'sprintingkiwi@gmail.com',
   url = 'https://github.com/sprintingkiwi/PYDOJO', # use the URL to the github repo
-  download_url = 'https://github.com/sprintingkiwi/PYDOJO/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/sprintingkiwi/PYDOJO/tarball/0.9', # I'll explain this in a second
   keywords = ['game', 'development', 'learning', 'education'], # arbitrary keywords
   classifiers = [],
 )
