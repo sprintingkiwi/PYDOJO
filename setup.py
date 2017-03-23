4cf6825be58a3bbf50d0e47ec346aa6eb943bed2
Alessandro Norfo <sprintingkiwi@gmail.com>
2017-03-23 15:08:16 +0100

    1.3 version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
     name='pydojo',
     packages=['pydojo'],
-    version='1.2',
+    version='1.3',
     description='A playful way to learn coding with Python',
     author='Alessandro Norfo',
     author_email='sprintingkiwi@gmail.com',
     url='https://github.com/sprintingkiwi/PYDOJO',
-    download_url='https://github.com/sprintingkiwi/PYDOJO/tarball/1.2',
+    download_url='https://github.com/sprintingkiwi/PYDOJO/tarball/1.3',
     keywords=['game', 'development', 'learning', 'education'],
     classifiers=[],
-    data_files=[('', ['pensurface.png', 'turtle.png'])],
-    include_package_data=True
+    include_package_data=True,
+    package_data={'pydojo': ['turtle.png', 'pensurface.png']}
 )
